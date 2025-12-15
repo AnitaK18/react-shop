c24ad6e664619d11c8a08a8adc9e42fe2a0f1d2d
AnitaK18 <117464090+AnitaK18@users.noreply.github.com>
2025-12-15 23:57:22 +0200

    fix: vite base url

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/react-shop",
+export default defineConfig(({ command }) => {
+  const base = command === "serve" ? "/" : "/react-shop";
+
+  return {
+    plugins: [react()],
+    base: base,
+  };
 });
